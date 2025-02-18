365da2dac69eb1864de62e22716c6cc5b9387401
TepidMilk <ivorykingrey@gmail.com>
2025-02-18 15:41:18 -0600

    Add Window Class

---
+++ src/window.py
@@ -0,0 +1,28 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window():
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.__root = Tk()
+        self.__root.title("TepidMilk's Maze Solver")
+        self.canvas = Canvas(self.__root, bd=self.width, height=self.height)
+        self.canvas.pack()
+        self.running = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+    
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+    
+    def wait_for_close(self):
+        self.running = True
+        """While the window is open continuously call redraw()"""
+        while self.running is True:
+            self.redraw()
+    
+    def close(self):
+        self.running = False
+
+
+
