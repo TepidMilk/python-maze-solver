9ddd3bbbd85edbb96c7d0402bb9315414322a4c5
TepidMilk <ivorykingrey@gmail.com>
2025-02-18 16:05:46 -0600

    Update main to use draw_line() method

--- src/main.py
+++ src/main.py
@@ -1,7 +1,13 @@
 from window import Window
+from point import Point
+from line import Line
 
 def main():
-    win = Window(400, 300)
+    win = Window(800, 600)
+    point1 = Point(0,0)
+    point2 = Point(800, 600)
+    line = Line(point1, point2)
+    win.draw_line(line, "black")
     win.wait_for_close()
 
 main()
