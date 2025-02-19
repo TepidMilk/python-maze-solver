94db70d7e061abdfc9fdd0eb389c3f321e5f0b0d
TepidMilk <ivorykingrey@gmail.com>
2025-02-18 19:25:10 -0600

    Update main to draw maze

--- src/main.py
+++ src/main.py
@@ -2,16 +2,11 @@ from window import Window
 from point import Point
 from line import Line
 from cell import Cell
+from maze import Maze
 
 def main():
     win = Window(800, 600)
-    cell1 = Cell(win)
-    cell2 = Cell(win)
-    cell3 = Cell(win)
-    cell1.draw(20, 20, 40, 40)
-    cell2.draw(40, 20, 60, 40)
-    cell3.draw(20, 40, 40, 60)
-    cell1.draw_move(cell2)
+    maze = Maze(20, 20, 5, 5, 20, 20, win)
     win.wait_for_close()
 
 main()
