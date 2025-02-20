90bb946e2f3457de61d55ed02dbf3a732f0862d7
TepidMilk <ivorykingrey@gmail.com>
2025-02-19 20:59:10 -0600

    Update main to call create maze

--- src/main.py
+++ src/main.py
@@ -7,6 +7,8 @@ from maze import Maze
 def main():
     win = Window(800, 600)
     maze = Maze(20, 20, 5, 5, 20, 20, win)
+    maze._create_cells()
+    maze._break_entrance_and_exit()
     win.wait_for_close()
 
 main()
