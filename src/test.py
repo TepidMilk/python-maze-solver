20ccc81f451d0339d4fd749748756b8021c93112
TepidMilk <ivorykingrey@gmail.com>
2025-02-20 13:21:37 -0600

    Add Maze method tests
    add tests for _reset_cells_visited Maze method

--- src/test.py
+++ src/test.py
@@ -45,6 +45,21 @@ class Tests(unittest.TestCase):
         with self.assertRaises(Exception):
             m4._create_cells()
 
+    def test_maze_reset_method(self):
+        num_cols = 12
+        num_rows = 10
+        m5 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        m5._create_cells()
+        m5._break_walls_r(0,0)
+        m5._reset_cells_visited()
+        for i in range(num_cols):
+            for j in range(num_rows):
+                self.assertEqual(m5._cells[i][j].visited, False)
+        
+    def test_empty_maze_reset(self):
+        m6 = Maze(0, 0, 0, 0, 10, 10)
+        m6._reset_cells_visited()
+
         
 
 if __name__ == "__main__":
