61478672197a480a9f651c9515eb593136969ab3
TepidMilk <ivorykingrey@gmail.com>
2025-02-19 20:58:36 -0600

    Add entrance and exit method

--- src/maze.py
+++ src/maze.py
@@ -12,35 +12,39 @@ class Maze():
         self._cell_size_y = cell_size_y
         self._win = win
         self._cells = []
-        self.current = None
     
     def _create_cells(self):
-        y_start = self._cell_size_y
         if self._num_cols < 2 or self._num_rows < 2:
             raise Exception("Invalid Maze Size")
         if self._cell_size_x < 10 or self._cell_size_y < 10:
             raise Exception("Invalid Cell Size")
-        for i in range(self._num_cols):
+        for num in range(self._num_cols):
             self._cells.append([Cell(self._win)] * self._num_rows)
-        for col in self._cells:
-            self._y1 = y_start
-            for cell in col:
-                self.current = cell
-                self._draw_cell(self._cell_size_x, self._cell_size_y)
-                self._y1 += self._cell_size_y
-            self._x1 += self._cell_size_x
+        for i in range(self._num_cols):
+            for j in range(self._num_rows):
+                self._draw_cell(i,j)
 
     def _draw_cell(self, i, j):
         if self._win == None:
             return
-        x1 = self._x1
-        y1 = self._y1
-        x2 = self._x1 + i
-        y2 = self._y1 + j
-        self.current.draw(x1, y1, x2, y2)
+        x1 = self._x1 + (i * self._cell_size_x)
+        y1 = self._y1 + (j * self._cell_size_y)
+        x2 = x1 + self._cell_size_x
+        y2 = y1 + self._cell_size_y
+        
+        self._cells[i][j].draw(x1, y1, x2, y2)
         self._animate()
     
     def _animate(self):
         self._win.redraw()
         time.sleep(0.1)
+
+    def _break_entrance_and_exit(self):
+        entrance = self._cells[0][0]
+        entrance.has_t_wall = False
+        self._draw_cell(0,0)
+        exit = self._cells[-1][-1]
+        exit.has_b_wall = False
+        self._draw_cell(self._num_cols - 1, self._num_rows - 1)
+
 
