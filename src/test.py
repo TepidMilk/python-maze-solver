9d700ada1a8ef0938113fff4327cae7a5efab27b
TepidMilk <ivorykingrey@gmail.com>
2025-02-19 20:16:17 -0600

    Update Tests to test broken logic

--- src/test.py
+++ src/test.py
@@ -7,6 +7,7 @@ class Tests(unittest.TestCase):
         num_cols = 12
         num_rows = 10
         m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        m1._create_cells()
         self.assertEqual(
             len(m1._cells),
             num_cols,
@@ -15,6 +16,36 @@ class Tests(unittest.TestCase):
             len(m1._cells[0]),
             num_rows,
         )
+    
+    def test_maze_create_many_cells(self):
+        num_cols = 1000
+        num_rows = 1000
+        m2 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        m2._create_cells()
+        self.assertEqual(
+            len(m2._cells),
+            num_cols,
+        )
+        self.assertEqual(
+            len(m2._cells[0]),
+            num_rows,
+        )
+    
+    def test_maze_invalid_maze_size(self):
+        num_cols = -12
+        num_rows = -10
+        m3 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        with self.assertRaises(Exception):
+            m3._create_cells()
+    
+    def test_maze_invalid_cell_size(self):
+        num_cols = 12
+        num_rows = 10
+        m4 = Maze(0, 0, num_rows, num_cols, 0, 0)
+        with self.assertRaises(Exception):
+            m4._create_cells()
+
+        
 
 if __name__ == "__main__":
     unittest.main()
