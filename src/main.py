af059f5e8e91639f4bc804d884ca9fae0028958f
TepidMilk <ivorykingrey@gmail.com>
2025-02-20 13:07:14 -0600

    Update main to use new maze methods

--- src/main.py
+++ src/main.py
@@ -6,9 +6,11 @@ from maze import Maze
 
 def main():
     win = Window(800, 600)
-    maze = Maze(20, 20, 5, 5, 20, 20, win)
+    maze = Maze(20, 20, 5, 5, 20, 20, win, 0)
     maze._create_cells()
     maze._break_entrance_and_exit()
+    maze._break_walls_r(0,0)
+    maze._reset_cells_visited()
     win.wait_for_close()
 
 main()
