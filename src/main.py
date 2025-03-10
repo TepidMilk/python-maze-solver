b991b34d61f23d33658991b1d746cc52fd1eb524
TepidMilk <ivorykingrey@gmail.com>
2025-03-10 17:23:42 -0500

    Update main to use solve_bfs()

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ def main():
     maze._break_entrance_and_exit()
     maze._break_walls_r(0,0)
     maze._reset_cells_visited()
-    maze.solve()
+    maze._solve_bsf()
     win.wait_for_close()
 
 main()
